2cba56554592333e717817c244298e9e6c087000
Harshita <chandaliyaharshita222@gmail.com>
2023-06-27 20:33:38 +0530

    If Else statements

---
+++ Programming_Constructs/IfandElse/EvenOrOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Constructs.IfandElse
+{
+    internal class EvenOrOdd
+    {
+        public void CheckOddEven()
+        {
+            Console.WriteLine("Program to check whether a given number is even or odd");
+            int num, rem;
+            Console.WriteLine("Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            rem = num % 2;
+            if (rem == 0)
+                Console.WriteLine("{0} is an Even number", num);
+            else
+                Console.WriteLine("{0} is an Odd number", num);
+        }
+    }
+}
---
+++ Programming_Constructs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Programming_Constructs.IfandElse;
+
+namespace Programming_Constructs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose any one from below options");
+            Console.WriteLine("1.Equal or not\n2.Even or Odd");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Equals eq = new Equals();
+                    eq.CheckEqualOrNot();
+                    break;
+                case 2:
+                    EvenOrOdd evenOrOdd = new EvenOrOdd();
+                    evenOrOdd.CheckOddEven();
+                    break;
+            }
+        }
+    }
+}
